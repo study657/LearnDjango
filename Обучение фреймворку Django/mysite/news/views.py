0ae2f4912bb205b08721ad77fdc2a2231b3acc03
study657 <79162199605@list.ru>
2022-02-04 22:10:23 +0300

    Шаблоны в Django

--- Обучение фреймворку Django/mysite/news/views.py
+++ Обучение фреймворку Django/mysite/news/views.py
@@ -1,9 +1,11 @@
 import imp
 from django.shortcuts import render
 from django.http import HttpResponse
+from news.models import News
 
 def index(request):
-    return HttpResponse('Hello world!')
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
 
 def test(request):
     return HttpResponse('<h1>Тестовая страница</h1>')
