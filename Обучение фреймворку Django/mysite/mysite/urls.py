3206fc2c768866fac7b4a902b62fb9b65a3ae418
study657 <79162199605@list.ru>
2022-02-04 21:38:42 +0300

    Миграции в Django

--- Обучение фреймворку Django/mysite/mysite/urls.py
+++ Обучение фреймворку Django/mysite/mysite/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from news.views import index, test
@@ -21,3 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
