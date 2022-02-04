dbd23f38dd91ead5ed4220f234f6a5147f4a7aa3
study657 <79162199605@list.ru>
2022-02-04 22:25:56 +0300

    Админка в Django

--- Обучение фреймворку Django/mysite/news/models.py
+++ Обучение фреймворку Django/mysite/news/models.py
@@ -1,9 +1,16 @@
+from tabnanny import verbose
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата правки')
     photo = models.ImageField(upload_to=f'photos/%Y/%m/%d')
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликованно')
+
+
+class Meta:
+    verbose_name = 'Новость'
+    verbose_name_plural = 'Новости'
+    ordering = ['-created_at']
