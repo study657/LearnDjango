4694c0c52c7772bd2da2514ad37e6b282309d60d
study657 <79162199605@list.ru>
2022-02-20 10:49:14 +0300

    Работа с формами. Часть 1

--- Обучение фреймворку Django/mysite/news/urls.py
+++ Обучение фреймворку Django/mysite/news/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('test/', test),
     path('category/<int:category_id>/', get_category, name='category'), # В таких скобочках <> записываются параметры, которые применяются в функции (<int:category_id>)
     path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/add_news/', add_news, name='add_news'),
 ]
--- Обучение фреймворку Django/mysite/news/views.py
+++ Обучение фреймворку Django/mysite/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from news.models import News, Category
+from .forms import NewsForm
 
 def index(request):
     news = News.objects.all()
@@ -18,3 +19,10 @@ def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {'news_item': news_item})
+
+def add_news(request):
+    if request.method == 'POST': # При отправке данных из формы
+        pass
+    else: # При рендеринге (показ формы при заходе на страницу)
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
