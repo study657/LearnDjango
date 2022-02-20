7b848b5d5a254ffd3e99e88b0b0efa9e77ffbf6c
study657 <79162199605@list.ru>
2022-02-20 17:03:02 +0300

    Работа с формами. Часть 2

--- Обучение фреймворку Django/mysite/news/views.py
+++ Обучение фреймворку Django/mysite/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from news.models import News, Category
 from .forms import NewsForm
@@ -22,7 +22,11 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == 'POST': # При отправке данных из формы
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            news = News.objects.create(**form.cleaned_data) # Сохранение данных в БД. ** - это распаковка словаря
+            return redirect(news) # После успешной отправки формы делаем редирект на страничку home или же на созданный объект, в данном случае на созданную новость
     else: # При рендеринге (показ формы при заходе на страницу)
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
