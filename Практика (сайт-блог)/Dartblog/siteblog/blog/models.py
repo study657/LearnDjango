48b933f5d69cd1c08dd54c28780ba1fb71a8fa79
study657 <79162199605@list.ru>
2022-04-13 10:30:58 +0300

    Разработано меню с помощью тегов шаблона

--- Практика (сайт-блог)/Dartblog/siteblog/blog/models.py
+++ Практика (сайт-блог)/Dartblog/siteblog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 """
 Category
@@ -30,6 +31,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"slug": self.slug})
+    
 
     class Meta:
         verbose_name = 'Категория'
--- Практика (сайт-блог)/Dartblog/siteblog/siteblog/settings.py
+++ Практика (сайт-блог)/Dartblog/siteblog/siteblog/settings.py
@@ -207,3 +207,11 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
+    }
+}
