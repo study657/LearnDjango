53de183c1472d39380ce4fa465d6f742e6e27938
study657 <79162199605@list.ru>
2022-04-18 21:01:51 +0300

    Реализовали поиск на сайте

--- Практика (сайт-блог)/Dartblog/siteblog/blog/views.py
+++ Практика (сайт-блог)/Dartblog/siteblog/blog/views.py
@@ -56,3 +56,17 @@ class PostByTag(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
         return context
+
+
+class Search(ListView):
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['s'] = f"s={self.request.GET.get('s')}&"
+        return context
